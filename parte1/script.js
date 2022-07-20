7899905dee5f9cc1a108e96ced8bc44926105e95
Gabriela França <gabrielafranca2008@hotmail.com>
2022-07-20 12:18:02 -0300

    Update script.js

--- parte1/script.js
+++ parte1/script.js
@@ -1,7 +1,7 @@
-const nome1 = prompt('Vou pedir informação sobre duas pessoas.\n Informe o nome da pessoa mais velha:');
-const idade1 = Number(prompt('Informe a idade da pessoa mais velha:'));
-const nome2 = prompt('Nome da pessoa mais nova:');
-const idade2 = Number(prompt('Idade da pessoa mais nova:'));
+const nome1 = prompt('Vou pedir informação sobre duas pessoas.\n Informe o NOME da pessoa mais velha:');
+const idade1 = Number(prompt('Informe a IDADE da pessoa mais velha:'));
+const nome2 = prompt('NOME da pessoa mais nova:');
+const idade2 = Number(prompt('IDADE da pessoa mais nova:'));
 
 const diferenca = idade1 - idade2;
 
